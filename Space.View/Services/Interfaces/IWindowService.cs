96716f6488e38c500251c59849b10aea7406a484
Dima <dmytro.hrechmak@nure.ua>
2022-01-04 01:52:26 +0200

    Added methods for closing and disabling windows

--- Space.View/Services/Interfaces/IWindowService.cs
+++ Space.View/Services/Interfaces/IWindowService.cs
@@ -5,5 +5,7 @@ namespace Space.View.Services.Interfaces
     public interface IWindowService
     {
         void ShowWindow(WindowType type);
+        void CloseWindow(WindowType type);
+        void LockWindow(WindowType type);
     }
 }
--- Space.View/Services/WindiwService.cs
+++ Space.View/Services/WindiwService.cs
@@ -7,10 +7,15 @@ namespace Space.View.Services
 {
     public class WindowService : IWindowService
     {
+        private FightWindow FightWnd;
+        private MarketWindow MarketWnd;
+
         public WindowService()
         {
             (Application.Current.Resources["Locator"] as ViewModelLocator).MainViewModel.NavigateToFightWindow += NavigateToFight;
             (Application.Current.Resources["Locator"] as ViewModelLocator).MainViewModel.NavigateToMarketWindow += NavigateToMarket;
+            (Application.Current.Resources["Locator"] as ViewModelLocator).MainViewModel.CloseFightWindow += OnCloseFightWindow;
+            (Application.Current.Resources["Locator"] as ViewModelLocator).MainViewModel.DisableMainWindow += OnDisableMainWindow;
         }
 
         public void ShowWindow(WindowType type)
@@ -18,14 +23,50 @@ namespace Space.View.Services
             switch (type)
             {
                 case WindowType.FightWindow:
-                    FightWindow FightWnd = new FightWindow();
+                    FightWnd = new FightWindow();
                     FightWnd.ShowDialog();
                     break;
                 case WindowType.MainWindow:
                     break;
                 case WindowType.MarketWindow:
-                    MarketWindow MarketWnd = new MarketWindow();
+                    MarketWnd = new MarketWindow();
                     MarketWnd.Show();
+                    break;
+                case WindowType.UpgradeWindow:
+                    break;
+            }
+        }
+
+        public void CloseWindow(WindowType type)
+        {
+            switch (type)
+            {
+                case WindowType.FightWindow:
+                    Application.Current.Dispatcher.Invoke(() => {
+                        FightWnd.Close();
+                    });
+                    break;
+                case WindowType.MainWindow:
+                    break;
+                case WindowType.MarketWindow:
+                    break;
+                case WindowType.UpgradeWindow:
+                    break;
+            }
+        }
+
+        public void LockWindow(WindowType type)
+        {
+            switch (type)
+            {
+                case WindowType.FightWindow:
+                    break;
+                case WindowType.MainWindow:
+                    Application.Current.Dispatcher.Invoke(() => {
+                        (App.Current.MainWindow as MainWindow).MainGrid.IsEnabled = false;
+                    });
+                    break;
+                case WindowType.MarketWindow:
                     break;
                 case WindowType.UpgradeWindow:
                     break;
@@ -42,6 +83,16 @@ namespace Space.View.Services
             ShowWindow(WindowType.MarketWindow);
         }
 
+        public void CloseFightWnd()
+        {
+            CloseWindow(WindowType.FightWindow);
+        }
+
+        public void DisableMainWindow()
+        {
+            LockWindow(WindowType.MainWindow);
+        }
+
         private void NavigateToFight(object sender, System.EventArgs e)
         {
             ShowFightWnd();
@@ -50,6 +101,16 @@ namespace Space.View.Services
         private void NavigateToMarket(object sender, System.EventArgs e)
         {
             ShowMarketWnd();
+        }
+
+        private void OnCloseFightWindow(object sender, System.EventArgs e)
+        {
+            CloseFightWnd();
+        }
+
+        private void OnDisableMainWindow(object sender, System.EventArgs e)
+        {
+            DisableMainWindow();
         }
     }
 }
